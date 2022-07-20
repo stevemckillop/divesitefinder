6d2de146e6d76b55e0737da6e1405d1ca31c8aa0
stevemckillop <92012639+stevemckillop@users.noreply.github.com>
2022-07-20 19:22:18 +0900

    feat: add nodemon running successfully

---
+++ src/data/index.js
@@ -0,0 +1,9 @@
+const { ishigaki } = require("./ishigaki.json");
+const { penida } = require("./penida.json");
+const { sealife } = require("./sealife.json");
+
+module.exports = {
+  ishigaki,
+  penida,
+  sealife,
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const { setupServer } = require("./server");
+const { setupServer } = require("./server.js");
 
 const server = setupServer();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log("Server listening on Port", PORT);
 });
