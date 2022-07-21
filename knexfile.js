4d9c9222160494d3171b7e897254a72ce18285c7
stevemckillop <92012639+stevemckillop@users.noreply.github.com>
2022-07-21 20:02:20 +0900

    feat: started KNEX file

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+
+module.exports = {
+    client: "pg",
+    connection: 
+        process.env.DATABASE_URL ||
+        process.env.DB_URL_LOCAL,
+    serchPath: "public",
+    migrations: {
+        tableName: "knex_migrations",
+        directory: __dirname + "/db/migrations"
+    },
+    seeds: {
+        directory: __dirname + "/db/seeds"
+    } 
+};
